3febf88c5dcff1988e3ca39e95746075756f3f3f
Nick Becker <nick@nbd.design>
2026-04-15 11:21:55 -0400

    Refactor AccountPage and SignInPage to improve session handling and user feedback. Implement loading states during membership verification and enhance error management for referral submissions. Update success messages for clearer communication on payment processing and membership status.

---
+++ shadcn-ui-kit-dashboard-main/app/dashboard/(auth)/apps/chat/page.tsx
@@ -0,0 +1,51 @@
+import { generateMeta } from "@/lib/utils";
+import path from "path";
+import { promises as fs } from "fs";
+import { ChatItemProps, UserPropsTypes } from "./types";
+
+import { ChatSidebar, ChatContent } from "./components";
+
+export async function generateMetadata() {
+  return generateMeta({
+    title: "Chat App",
+    additionalTitle: true,
+    description:
+      "Manage real-time conversations, media sharing, and contact lists with a modern messaging ui. A professional chat application page built with React, Next.js, TypeScript, Tailwind CSS, and shadcn/ui.",
+    canonical: "/apps/chat"
+  });
+}
+
+async function getChats() {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "app/dashboard/(auth)/apps/chat/data/chats.json")
+  );
+  return JSON.parse(data.toString());
+}
+
+async function getChatUser(id: number) {
+  const data = await fs.readFile(
+    path.join(process.cwd(), "app/dashboard/(auth)/apps/chat/data/contacts.json")
+  );
+
+  return JSON.parse(data.toString()).find((item: UserPropsTypes) => item.id === id);
+}
+
+export default async function Page() {
+  const chats = await getChats();
+
+  const chats_with_user = await Promise.all(
+    chats.map(async (item: ChatItemProps) => {
+      item.user = await getChatUser(item.user_id);
+      return item;
+    })
+  );
+
+  return (
+    <div className="flex h-[calc(100vh-var(--header-height)-3rem)] w-full">
+      <ChatSidebar chats={chats_with_user} />
+      <div className="grow">
+        <ChatContent />
+      </div>
+    </div>
+  );
+}
